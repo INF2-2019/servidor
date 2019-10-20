650b971032f3da2b7beae95bab97504173a774c7
Alface <pedrocesar1410@gmail.com>
2019-10-20 00:21:18 -0300

    Renomear ConsultarDepartamento

--- app/src/java/controller/diario/departamentos/ConsultarDepartamentos.java
+++ app/src/java/controller/diario/departamentos/ConsultarDepartamentos.java
@@ -13,63 +13,43 @@ import model.diario.departamentos.Departamento;
 import repository.diario.DepartamentoRepository;
 import view.diario.DepartamentoView;
 
-@WebServlet(name = "Consultar", urlPatterns = "/diario/departamentos/consultar")
-public class Consultar extends HttpServlet {
+@WebServlet(name = "ConsultarDepartamentos", urlPatterns = "/diario/departamentos/ConsultarDepartamentos")
+public class ConsultarDepartamentos extends HttpServlet {
 
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		response.setContentType("text/xml;charset=UTF-8");
-		
+		response.addHeader("Access-Control-Allow-Origin", "*");
+
 		try(PrintWriter out = response.getWriter()) {
-			
+
 			try {
 				List<Departamento> deptos = DepartamentoRepository.consulta();
 				String xml = DepartamentoView.xmlDepartamentos(deptos);
 				out.println(xml);
 			} catch(SQLException ex) {
-				out.println("<erro>" + ex + "</erro>");
+				System.err.println(ex);
 			}
-			
+
 		}
 	}
 
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
-	 * Handles the HTTP <code>GET</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	/**
-	 * Handles the HTTP <code>POST</code> method.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
 	@Override
 	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
+		return "Servlet que consulta departamentos.";
+	}
 
 }
