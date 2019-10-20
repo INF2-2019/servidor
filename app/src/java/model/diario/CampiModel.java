bc604f3d821055ead4d4f9040d93160a9f68809c
Pedro <pedro.gandra.andrade@gmail.com>
2019-10-20 14:16:55 -0300

    Criação CampiModel

---
+++ app/src/java/model/diario/CampiModel.java
@@ -0,0 +1,55 @@
+package model.diario;
+
+import model.Model;
+import static model.Model.ID_INDEFINIDO;
+
+public class CampiModel extends Model {
+	private int id;
+	private String nome, cidade, uf;
+
+	public CampiModel(String nome, String cidade, String uf){
+		// Para evitar repetição, reaproveitamento do construtor completo
+		this(ID_INDEFINIDO, nome, cidade, uf);
+	}
+
+	public CampiModel(int id, String nome, String cidade, String uf){
+		// Usar setters para validar os dados de maneira isolada
+		this.setId(id);		
+		this.setNome(nome);
+		this.setCidade(cidade);
+		this.setUf(uf);
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+}
