f4fb0a9670848e8e65bda408b46c4aece4f2e267
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2019-10-20 10:22:08 -0300

    Update AtualizarDisciplinas.java

--- app/src/java/controller/diario/disciplinas/AtualizarDisciplinas.java
+++ app/src/java/controller/diario/disciplinas/AtualizarDisciplinas.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller.diario.disciplinas;
 
 import java.io.IOException;
@@ -13,11 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author User
- */
-@WebServlet(name = "AtualizarDisciplinas", urlPatterns = {"/diario/disciplinas/atualizar"})
+@WebServlet(name = "AtualizarDisciplinas", urlPatterns = "/diario/disciplinas/atualizar")
 public class AtualizarDisciplinas extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response){}
 }
