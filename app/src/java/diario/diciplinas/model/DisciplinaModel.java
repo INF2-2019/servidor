46a4d7172d87b0edde36909285df1817365e5994
MarcusMoraes <marcussviniciusss123@gmail.com>
2019-10-21 18:45:47 -0300

    Modelo da tabela Disciplinas

---
+++ app/src/java/diario/diciplinas/model/DisciplinaModel.java
@@ -0,0 +1,48 @@
+package diario.disciplinas.model;
+
+
+import diario.disciplinas.model.Model;
+
+public class DisciplinaModel extends Model {
+	private int id, idTurmas, cargaHorariaMin;
+	private String nome;
+
+    public DisciplinaModel(int id,int idTurmas, String nome,int cargaHorariaMin) {
+        this.id = id;
+        this.idTurmas = idTurmas;
+        this.cargaHorariaMin = cargaHorariaMin;
+        this.nome = nome;
+    }
+
+    public DisciplinaModel(int idTurmas, int cargaHorariaMin, String nome) {
+        this(ID_INDEFINIDO, idTurmas,nome,cargaHorariaMin);
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public int getIdTurmas() {
+        return idTurmas;
+    }
+
+    public void setIdTurmas(int idTurmas) {
+        this.idTurmas = idTurmas;
+    }
+
+    public int getCargaHorariaMin() {
+        return cargaHorariaMin;
+    }
+
+    public void setCargaHorariaMin(int cargaHorariaMin) {
+        this.cargaHorariaMin = cargaHorariaMin;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
