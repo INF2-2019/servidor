0ff1229d8fd22183d740c1b889b4679094683cba
MarcusMoraes <marcussviniciusss123@gmail.com>
2019-10-21 20:00:28 -0300

    Rota para consultar por ID

---
+++ app/src/java/diario/disciplinas/ConsultarPorId.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diario.disciplinas;
+
+import diario.disciplinas.model.DisciplinaModel;
+import diario.disciplinas.repository.DisciplinaRepository;
+import diario.disciplinas.views.DisciplinaConsultaView;
+import diario.disciplinas.views.ErroView;
+import diario.disciplinas.views.RenderException;
+import diario.disciplinas.views.View;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import utils.ConnectionFactory;
+import utils.Headers;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(name = "ConsultarPorId", urlPatterns = {"/diario/disciplinas/consultarporid"})
+public class ConsultarPorId extends HttpServlet {
+     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            Headers.XMLHeaders(response);
+            Connection conexao = ConnectionFactory.getDiario();
+            PrintWriter out = response.getWriter();
+            if(conexao == null){
+                System.err.println("Falha ao conectar ao bd");
+                View erroView = new ErroView(new Exception("Não foi possível conectar ao banco de dados"));
+                try {
+                    erroView.render(out);
+                } catch (RenderException e) {
+                    throw new ServletException(e);
+                }
+                return;
+            }
+            DisciplinaRepository DisciplinaRep = new DisciplinaRepository(conexao);
+            Set<DisciplinaModel> resultado;
+            try{
+                resultado = new HashSet<>();                        
+                resultado.add(DisciplinaRep.consultarId(request.getParameter("id")));
+                 View DisciplinaConsultaView = new DisciplinaConsultaView(resultado);
+                 DisciplinaConsultaView.render(out);
+            } catch(NumberFormatException excecaoFormatoErrado) {
+                    response.setStatus(400);
+                    System.err.println("Número inteiro inválido para o parâmetro. Erro: "+excecaoFormatoErrado.toString());
+                    View erroView = new ErroView(excecaoFormatoErrado);
+                try {
+                     erroView.render(out);
+                } catch (RenderException e) {
+                    throw new ServletException(e);
+                }
+            } catch(SQLException excecaoSQL) {
+                response.setStatus(400);
+                System.err.println("Busca SQL inválida. Erro: "+excecaoSQL.toString());
+                View erroView = new ErroView(excecaoSQL);
+                try {
+                    erroView.render(out);
+                } catch (RenderException e) {
+                    throw new ServletException(e);
+                }
+            try{
+                conexao.close();
+            } catch(SQLException erro) {
+                System.err.println("Erro ao fechar banco de dados. Erro: "+erro.toString());
+			}
+            } catch (RenderException ex) {
+            Logger.getLogger(ConsultarDisciplinas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    
+    }
+}
