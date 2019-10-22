3826be9e9987a538afa0579b1c2f5a60ed08fb54
Chules <hiiamchules@gmail.com>
2019-10-22 15:06:08 -0300

    Views para exibir mensagens de erro ou sucesso das operações

---
+++ app/src/java/diario/etapas/view/View.java
@@ -0,0 +1,25 @@
+package diario.etapas.view;
+
+import diario.etapas.RenderException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import diario.etapas.RenderException;
+
+public abstract class View<D> {
+
+    protected D data;
+
+    public View(D d) {
+	this.data = d;
+    }
+
+    public abstract void render(PrintWriter writer) throws RenderException;
+
+    public String render() throws RenderException {
+	StringWriter data = new StringWriter();
+	PrintWriter printWriter = new PrintWriter(data);
+	render(printWriter);
+	return data.toString();
+    }
+;
+}
