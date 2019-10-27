1268ea820f2ab4fe86957c2c7601513e861dfa75
Alface <pedrocesar1410@gmail.com>
2019-10-26 22:28:19 -0300

    Correcao de erros e formatacao
    Correcao de erro na consulta por id e substituicao de espacos por tabs

--- app/src/java/diario/departamentos/repository/DepartamentoRepository.java
+++ app/src/java/diario/departamentos/repository/DepartamentoRepository.java
@@ -12,163 +12,151 @@ import utils.ConnectionFactory;
 
 public class DepartamentoRepository {
 
-    public static List<Departamento> consulta() throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){    
-            Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM `departamentos`");
-
-			List<Departamento> deptos = new LinkedList();
-
-			while(rs.next()) {
-				deptos.add(new Departamento(rs.getInt("id"), rs.getInt("id-campi"), rs.getString("nome")));
+	public static List<Departamento> consulta() throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			List<Departamento> deptos;
+			try(Statement stmt = con.createStatement()) {
+				ResultSet rs = stmt.executeQuery("SELECT * FROM `departamentos`");
+				deptos = new LinkedList();
+				while(rs.next()) {
+					deptos.add(new Departamento(rs.getInt("id"), rs.getInt("id-campi"), rs.getString("nome")));
+				}
 			}
-                
-            stmt.close();
-            con.close();
-            return deptos;
-        }
-        else{
-            throw new SQLException();
-        }
-    }
+			con.close();
+			return deptos;
+		} else {
+			throw new SQLException();
+		}
+	}
 
-    public static Departamento consulta(int id) throws SQLException {
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("SELECT * FROM `departamentos` WHERE `id` = ?");
-            prst.setInt(1, id);
-			ResultSet rs = prst.executeQuery();
-            
-            Departamento depto = new Departamento(rs.getInt("id"), rs.getInt("idCampi"), rs.getString("nome"));
-            
-			prst.close();
+	public static Departamento consulta(int id) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			Departamento depto;
+			try(PreparedStatement prst = con.prepareStatement("SELECT * FROM `departamentos` WHERE `id` = ?")) {
+				prst.setInt(1, id);
+				ResultSet rs = prst.executeQuery();
+				rs.next();
+				depto = new Departamento(rs.getInt("id"), rs.getInt("id-campi"), rs.getString("nome"));
+			}
 			con.close();
 
-            return depto;
-        }
-        else{
-            throw new SQLException();
-        }
-    }
+			return depto;
+		} else {
+			throw new SQLException();
+		}
+	}
 
-    public static void insere(int idCampi, String nome) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("INSERT INTO `departamentos` (`id-campi`, `nome`) VALUES (?, ?)");
-			prst.setInt(1, idCampi);
-			prst.setString(2, nome);
-            prst.executeUpdate();
-            prst.close();
-            con.close();;
-        }
-        else{
-            throw new SQLException();
-        }
-    }
-
-    public static void insere(Departamento depto) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("INSERT INTO `departamentos` (`id-campi`, `nome`) VALUES (?, ?)");
-			prst.setInt(1, depto.getIdCampi());
-			prst.setString(2, depto.getNome());
-            prst.executeUpdate();
-            prst.close();
-            con.close();
-        }
-        else{
-            throw new SQLException();
-        }
-    }
-
-    public static void atualiza(int id, int idCampi) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `id-campi` = ? WHERE `id` = ?");
-			prst.setInt(1, idCampi);
-			prst.setInt(2, id);
-			prst.executeUpdate();
-			prst.close();
+	public static void insere(int idCampi, String nome) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("INSERT INTO `departamentos` (`id-campi`, `nome`) VALUES (?, ?)")) {
+				prst.setInt(1, idCampi);
+				prst.setString(2, nome);
+				prst.executeUpdate();
+			}
 			con.close();
-        }
-        else{
-           throw new SQLException();
-        }
-    }
+		} else {
+			throw new SQLException();
+		}
+	}
 
-    public static void atualiza(int id, String nome) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `nome` = ? WHERE `id` = ?");
-			prst.setString(1, nome);
-			prst.setInt(2, id);
-			prst.executeUpdate();
-			prst.close();
+	public static void insere(Departamento depto) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("INSERT INTO `departamentos` (`id-campi`, `nome`) VALUES (?, ?)")) {
+				prst.setInt(1, depto.getIdCampi());
+				prst.setString(2, depto.getNome());
+				prst.executeUpdate();
+			}
 			con.close();
-        }
-        else{
-            throw new SQLException();
-        }
-    }
+		} else {
+			throw new SQLException();
+		}
+	}
 
-    public static void atualiza(int id, int idCampi, String nome) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `id-campi` = ?, `nome` = ? WHERE `id` = ?");
-			prst.setInt(1, idCampi);
-			prst.setString(2, nome);
-			prst.setInt(3, id);
-            prst.executeUpdate();
-			prst.close();
+	public static void atualiza(int id, int idCampi) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `id-campi` = ? WHERE `id` = ?")) {
+				prst.setInt(1, idCampi);
+				prst.setInt(2, id);
+				prst.executeUpdate();
+			}
 			con.close();
-        }
-        else{
-            throw new SQLException();
-        }
-    }
+		} else {
+			throw new SQLException();
+		}
+	}
 
-    public static void atualiza(Departamento depto) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `id-campi` = ?, `nome` = ? WHERE `id` = ?");
-			prst.setInt(1, depto.getIdCampi());
-			prst.setString(2, depto.getNome());
-			prst.setInt(3, depto.getId());
-			prst.executeUpdate();
-			prst.close();
+	public static void atualiza(int id, String nome) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `nome` = ? WHERE `id` = ?")) {
+				prst.setString(1, nome);
+				prst.setInt(2, id);
+				prst.executeUpdate();
+			}
 			con.close();
-        }
-        else{
-            throw new SQLException();
-        }
-    }
+		} else {
+			throw new SQLException();
+		}
+	}
 
-    public static void remove(int id) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("DELETE FROM `departamentos` WHERE `id` = ?");
-			prst.setInt(1, id);
-            prst.executeUpdate();
-			prst.close();
-			con.close();     
-        }
-        else{
-            throw new SQLException();
-        }
-    }
-
-    public static void remove(Departamento depto) throws SQLException{
-        Connection con = ConnectionFactory.getDiario();
-        if(con != null){
-            PreparedStatement prst = con.prepareStatement("DELETE FROM `departamentos` WHERE `id` = ?");
-			prst.setInt(1, depto.getId());
-			prst.executeUpdate();
-			prst.close();
+	public static void atualiza(int id, int idCampi, String nome) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `id-campi` = ?, `nome` = ? WHERE `id` = ?")) {
+				prst.setInt(1, idCampi);
+				prst.setString(2, nome);
+				prst.setInt(3, id);
+				prst.executeUpdate();
+			}
 			con.close();
-        }
-        else{
-            throw new SQLException();
-        }
-    }
+		} else {
+			throw new SQLException();
+		}
+	}
+
+	public static void atualiza(Departamento depto) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("UPDATE `departamentos` SET `id-campi` = ?, `nome` = ? WHERE `id` = ?")) {
+				prst.setInt(1, depto.getIdCampi());
+				prst.setString(2, depto.getNome());
+				prst.setInt(3, depto.getId());
+				prst.executeUpdate();
+			}
+			con.close();
+		} else {
+			throw new SQLException();
+		}
+	}
+
+	public static void remove(int id) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("DELETE FROM `departamentos` WHERE `id` = ?")) {
+				prst.setInt(1, id);
+				prst.executeUpdate();
+			}
+			con.close();
+		} else {
+			throw new SQLException();
+		}
+	}
+
+	public static void remove(Departamento depto) throws SQLException {
+		Connection con = ConnectionFactory.getDiario();
+		if(con != null) {
+			try(PreparedStatement prst = con.prepareStatement("DELETE FROM `departamentos` WHERE `id` = ?")) {
+				prst.setInt(1, depto.getId());
+				prst.executeUpdate();
+			}
+			con.close();
+		} else {
+			throw new SQLException();
+		}
+	}
 }
