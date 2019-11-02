bb1fe64ef06438a7d21b03ce29cc9ccbba96b1b7
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2019-11-02 12:01:56 -0300

    fix NotAuthorizedException

--- app/src/java/diario/professores/LogarProfessor.java
+++ app/src/java/diario/professores/LogarProfessor.java
@@ -15,7 +15,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.NotAuthorizedException;
 
 /**
  * <h1> Servlet de Log-in</h1>
