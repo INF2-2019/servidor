6433c8699e473fec5c4a5b215a4ebc4f9562402a
MarcusMoraes <marcussviniciusss123@gmail.com>
2019-11-03 01:37:55 -0300

    Model de Reserva

---
+++ app/src/java/biblioteca/reservas/model/Model.java
@@ -0,0 +1,7 @@
+
+package biblioteca.reservas.model;
+
+public abstract class Model {
+
+    public final static int ID_INDEFINIDO = -1;
+}
---
+++ app/src/java/biblioteca/reservas/model/ReservaModel.java
@@ -0,0 +1,108 @@
+package biblioteca.reservas.model;
+
+import java.util.Date;
+import java.util.Map;
+
+public class ReservaModel extends Model {
+
+	int id, idAlunos, idAcervo, TempoEspera;
+	Date dataReserva;
+	boolean emprestou;
+
+	public ReservaModel(int id, int idAlunos, int idAcervo, int TempoEspera, Date dataReserva, boolean emprestou) {
+		this.id = id;
+		this.idAlunos = idAlunos;
+		this.idAcervo = idAcervo;
+		this.TempoEspera = TempoEspera;
+		this.dataReserva = dataReserva;
+		this.emprestou = emprestou;
+	}
+
+	public Object[] retornarValoresRestantes(Map<String, String> parametros) {
+		Object[] retorno = new Object[6];
+		retorno[0] = id;
+
+		if (!parametros.containsKey("id-alunos")) {
+			retorno[1] = idAlunos;
+		} else {
+			retorno[1] = parametros.get("id-alunos");
+		}
+
+		if (!parametros.containsKey("id-acervo")) {
+			retorno[2] = idAcervo;
+		} else {
+			retorno[2] = parametros.get("id-acervo");
+		}
+		if (!parametros.containsKey("tempo-espera")) {
+			retorno[3] = TempoEspera;
+		} else {
+			retorno[3] = parametros.get("tempo-espera");
+		}
+		if (!parametros.containsKey("data-reserva")) {
+			retorno[4] = dataReserva;
+		} else {
+			retorno[4] = parametros.get("data-reserva");
+		}
+		if (!parametros.containsKey("emprestou")) {
+			retorno[5] = dataReserva;
+		} else {
+			retorno[5] = parametros.get("emprestou");
+		}
+
+		return retorno;
+	}
+
+	public ReservaModel(int idAlunos, int idAcervo, int TempoEspera, Date dataReserva, boolean emprestou) {
+		this.id = ID_INDEFINIDO;
+		this.idAlunos = idAlunos;
+		this.idAcervo = idAcervo;
+		this.TempoEspera = TempoEspera;
+		this.dataReserva = dataReserva;
+		this.emprestou = emprestou;
+	}
+
+	public int getId() {
+		return id;
+	}
+ 
+	public int getIdAlunos() {
+		return idAlunos;
+	}
+
+	public void setIdAlunos(int idAlunos) {
+		this.idAlunos = idAlunos;
+	}
+
+	public int getIdAcervo() {
+		return idAcervo;
+	}
+
+	public void setIdAcervo(int idAcervo) {
+		this.idAcervo = idAcervo;
+	}
+
+	public int getTempoEspera() {
+		return TempoEspera;
+	}
+
+	public void setTempoEspera(int TempoEspera) {
+		this.TempoEspera = TempoEspera;
+	}
+
+	public Date getDataReserva() {
+		return dataReserva;
+	}
+
+	public void setDataReserva(Date dataReserva) {
+		this.dataReserva = dataReserva;
+	}
+
+	public boolean isEmprestou() {
+		return emprestou;
+	}
+
+	public void setEmprestou(boolean emprestou) {
+		this.emprestou = emprestou;
+	}
+
+}
