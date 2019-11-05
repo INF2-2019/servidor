a6535049b3f447a2b8a2a8cbddc8501162b96b79
MarcusMoraesOliveira <41203422+MarcusMoraesOliveira@users.noreply.github.com>
2019-11-05 12:30:49 -0300

    Update CargoLogadoController.java

--- app/src/java/biblioteca/admin/controllers/CargoLogadoController.java
+++ app/src/java/biblioteca/admin/controllers/CargoLogadoController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 @WebServlet(name = "CargoLogadoControllerBiblioteca", urlPatterns = "/biblioteca/cargo")
 public class CargoLogadoController extends HttpServlet {
 
