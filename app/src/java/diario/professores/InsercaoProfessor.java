ea9a7db2ae832cf64ec7c948699fde959e24b005
Jônata <40151891+Jonatanc05@users.noreply.github.com>
2019-11-11 15:38:13 -0300

    Usando sugestão #2
    Co-Authored-By: MarcusMoraesOliveira <41203422+MarcusMoraesOliveira@users.noreply.github.com>

--- app/src/java/diario/professores/InsercaoProfessor.java
+++ app/src/java/diario/professores/InsercaoProfessor.java
@@ -67,7 +67,7 @@ public class InsercaoProfessor extends HttpServlet {
 			saida.println("</sucesso>");
 
 		} catch (ExcecaoParametrosIncorretos e) {
-			resposta.setStatus(400);
+			resposta.setStatus(422);
 			saida.println("<erro><mensagem>" + e.getMessage() + "</mensagem></erro>");
 		} catch (ExcecaoNaoAutorizado e) {
 			resposta.setStatus(403);
