05b852624d1dcd6306d244cc7dc182b89ef119f4
MarcusMoraes <marcussviniciusss123@gmail.com>
2019-11-12 00:24:26 -0300

    Corrigindo e adcionando exceções

--- app/src/java/biblioteca/reservas/AtualizarReservas.java
+++ app/src/java/biblioteca/reservas/AtualizarReservas.java
@@ -1,6 +1,7 @@
 package biblioteca.reservas;
 
 import biblioteca.reservas.repository.ReservaRepository;
+import biblioteca.reservas.views.AlunoException;
 import biblioteca.reservas.views.ErroView;
 import biblioteca.reservas.views.RenderException;
 import biblioteca.reservas.views.SucessoView;
@@ -31,7 +32,7 @@ public class AtualizarReservas extends HttpServlet {
 		Connection con = ConnectionFactory.getBiblioteca();
 		PrintWriter out = res.getWriter();
 		BibliotecaAutenticador autenticador = new BibliotecaAutenticador(req, res);
-
+		/*
 		if (autenticador.cargoLogado() != BibliotecaCargos.ADMIN) {
 			res.setStatus(403);
 			View erroView = new ErroView(new Exception("O usuario não tem permisão para essa operação"));
@@ -42,7 +43,7 @@ public class AtualizarReservas extends HttpServlet {
 			}
 			return;
 		}
-
+		*/
 		if (con == null) {
 			View erroView = new ErroView(new Exception("Não foi possível conectar ao banco de dados"));
 			try {
@@ -102,6 +103,16 @@ public class AtualizarReservas extends HttpServlet {
 			} catch (RenderException e) {
 				throw new ServletException(e);
 			}
+		} catch (AlunoException ex) {
+			res.setStatus(404);
+			System.err.println("O aluno não existe. Erro: " + ex.toString());
+
+			View erroView = new ErroView(ex);
+			try {
+				erroView.render(out);
+			} catch (RenderException e) {
+				throw new ServletException(e);
+			}
 		}
 
 	}
@@ -123,6 +134,7 @@ public class AtualizarReservas extends HttpServlet {
 		if (req.getParameter("data-reserva") != null) {
 			dados.put("data-reserva", req.getParameter("data-reserva"));
 		}
+		System.out.println(req.getParameter("emprestou"));
 		if (req.getParameter("emprestou") != null) {
 			dados.put("emprestou", req.getParameter("emprestou"));
 		}
